80a083fd27c070666e11d4a0bbec1ffebf74a2c9
yhkekeke <diandian2510@gmail.com>
2024-01-14 13:05:45 +0800

    create ALL pages in views , images(in assets) , scss(in assets).

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <script>
-const imgUrl = new URL('../src/assets/images/index/讚.jpg', import.meta.url).href
+const imgUrl = new URL('../src/assets/images/讚.jpg', import.meta.url).href
 export default {
   data() {
     return {
@@ -21,21 +21,30 @@ export default {
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <img :src="imgUrl" alt="">
+    <img :src="imgUrl" alt="" width="200" height="200">
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="WELCOME PXZoO" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        
-        <RouterLink to="/product">Product</RouterLink>
+        <RouterLink to="/about">About</RouterLink> <!-- 這個我們沒有，之後刪掉 -->
+        <RouterLink to="/ticket">Ticket</RouterLink>
+        <RouterLink to="/member">Member</RouterLink>
         <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/animal">Animal</RouterLink>
+        <RouterLink to="/park">Park</RouterLink>
+        <RouterLink to="/news">News</RouterLink>
+        <RouterLink to="/school">School</RouterLink>
+        <RouterLink to="/vote">Vote</RouterLink>
       </nav>
     </div>
   </header>
 
   <RouterView />
+
+  <footer>
+    版權文字請貼這裡
+  </footer>
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+// 新增路由都用下面的寫法，不要用上面這個
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,20 +11,50 @@ const router = createRouter({
       component: HomeView
     },
     {
+      // 這個我們沒有，之後刪掉
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/product',
-      name: 'product',
-      component: () => import('../views/ProductView.vue')
-    }
-    ,
+      path: '/ticket',
+      name: 'ticket',
+      component: () => import('../views/TicketView.vue')
+    },
+    {
+      path: '/member',
+      name: 'member',
+      component: () => import('../views/MemberView.vue')
+    },
     {
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/animal',
+      name: 'animal',
+      component: () => import('../views/AnimalView.vue')
+    },
+    {
+      path: '/park',
+      name: 'park',
+      component: () => import('../views/ParkView.vue')
+    },
+    {
+      path: '/news',
+      name: 'news',
+      component: () => import('../views/NewsView.vue')
+    },
+    {
+      path: '/school',
+      name: 'school',
+      component: () => import('../views/SchoolView.vue')
+    },
+    {
+      path: '/vote',
+      name: 'vote',
+      component: () => import('../views/VoteView.vue')
     }
   ]
 })
