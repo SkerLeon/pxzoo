857717723379a4e614a1744cdf342a3d9dfc66c6
yhkekeke <diandian2510@gmail.com>
2024-01-14 11:26:25 +0800

    create folders 'images'&'scss', and 'App.vue'add img

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,25 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+<script>
+const imgUrl = new URL('../src/assets/images/讚.jpg', import.meta.url).href
+export default {
+  data() {
+    return {
+      rawHtml: '<p>1</p>',
+      imgUrl: imgUrl
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <img :src="imgUrl" alt="">
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
