99fc6bc3820b52849f919c246d2b681e496f605e
Hsuan1128 <dewitt1128@gmail.com>
2024-01-19 21:14:35 +0800

    add login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,56 +10,95 @@
     <div class="cloudSun">
       <img src="/public/images/login/cloudsun.svg" alt="" />
     </div>
-    <form class="signup active" id="loginForm">
-      <div class="loginBtnGroup">
-        <button class="loginBtn pcSmTitle active">會員登入</button>
-        <button class="signupBtn pcSmTitle">註冊會員</button>
-      </div>
-      <div class="loginInputGroup">
-        <div class="acc">
-          <div class="img">
-            <img src="/public/images/login/icon/account.svg" alt="" />
-          </div>
-
-          <input type="text" placeholder="帳號" class="pcMarkText" />
+    <div>
+      <div class="loginSingupArea">
+        <div class="loginBtnGroup">
+          <button
+            class="loginBtn pcSmTitle"
+            @click="activeTab = 'loginForm'"
+            :class="{ active: activeTab === 'loginForm' }"
+          >
+            會員登入
+          </button>
+          <button
+            class="signupBtn pcSmTitle"
+            @click="activeTab = 'signForm'"
+            :class="{ active: activeTab === 'signForm' }"
+          >
+            註冊會員
+          </button>
         </div>
-        <div class="psw">
-          <div class="img">
-            <img src="/public/images/login/icon/psw.svg" alt="" />
-          </div>
-          <input type="password" placeholder="密碼" />
-        </div>
-        <button class="defaultBtn">
-          登入
-          <img src="/public/images/login/icon/btnArrow.svg" alt="" />
-        </button>
-        <myButton />
-      </div>
-    </form>
+        <form class="myLog" id="loginForm" v-show="activeTab === 'loginForm'">
+          <!-- <div class="loginBtnGroup">
+            <button class="loginBtn pcSmTitle active">會員登入</button>
+            <button class="signupBtn pcSmTitle">註冊會員</button>
+          </div> -->
+          <div class="loginInputGroup">
+            <div class="acc">
+              <div class="img">
+                <img src="/public/images/login/icon/account.svg" alt="" />
+              </div>
 
-    <!-- <form class="signup" id="signForm" style="display: none">
-      <div class="loginBtnGroup">
-        <button class="loginBtn pcInnerText">會員登入</button>
-        <button class="signupBtn pcSmTitle active">註冊會員</button>
+              <input type="text" placeholder="帳號" class="pcMarkText" />
+            </div>
+            <div class="psw">
+              <div class="img">
+                <img src="/public/images/login/icon/psw.svg" alt="" />
+              </div>
+              <input type="password" placeholder="密碼" />
+            </div>
+            <button class="defaultBtn">
+              登入
+              <img src="/public/images/login/icon/btnArrow.svg" alt="" />
+            </button>
+          </div>
+        </form>
+
+        <form class="myLog" id="signForm" v-show="activeTab === 'signForm'">
+          <div class="loginInputGroup">
+            <div class="acc">
+              <div class="img">
+                <img src="/public/images/login/icon/account.svg" alt="" />
+              </div>
+
+              <input type="text" placeholder="帳號" class="pcMarkText" />
+            </div>
+            <div class="acc">
+              <div class="img">
+                <img src="/public/images/login/icon/account.svg" alt="" />
+              </div>
+
+              <input type="text" placeholder="帳號" class="pcMarkText" />
+            </div>
+            <div class="psw">
+              <div class="img">
+                <img src="/public/images/login/icon/psw.svg" alt="" />
+              </div>
+              <input type="password" placeholder="密碼" />
+            </div>
+            <div class="psw">
+              <div class="img">
+                <img src="/public/images/login/icon/psw.svg" alt="" />
+              </div>
+              <input type="password" placeholder="密碼" />
+            </div>
+            <button class="defaultBtn">
+              註冊
+              <img src="/public/images/login/icon/btnArrow.svg" alt="" />
+            </button>
+          </div>
+        </form>
       </div>
-      <input type="text" />
-      <input type="text" />
-      <input type="password" />
-      <input type="password" />
-      <myButton />
-    </form> -->
+    </div>
   </section>
 </template>
 
 <script>
-import myButton from "@/components/btn.vue";
 export default {
-  components: {
-    myButton,
-  },
   data() {
-    return {};
+    return {
+      activeTab: "loginForm", // 默认显示登录表单
+    };
   },
-  methods: {},
 };
 </script>
