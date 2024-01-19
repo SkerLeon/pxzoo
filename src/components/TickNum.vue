b2cb50a4f00c57821ecc181292584e98392f3951
yhkekeke <diandian2510@gmail.com>
2024-01-20 03:07:45 +0800

    update ticket

---
+++ src/components/TickNum.vue
@@ -0,0 +1,119 @@
+<template>
+    <section class="tickNum">
+
+
+    <!-- part2 選擇數量 -->
+        <hgroup>
+            <h2>選擇數量</h2>
+            <img src="/images/ticket/refresh.svg">
+        </hgroup>
+
+        <article v-if="isMobile">
+            <div v-for=" ticket in tickets" :key="ticket.id">
+                <div>
+                    <p>{{ ticket.name }}</p>
+                    <p>{{ ticket.rule }}</p>
+                </div>
+                <div>
+                    <h2>NT$ {{ ticket.price }}</h2>
+                    <p>/ 人</p>
+                    <div>
+                    <button>+</button>
+                    <input type="number" name="0" id="tickNumber">
+                    <button>-</button>
+                </div>
+                </div>
+            </div>
+        </article>
+        <div v-else>
+            <article v-for=" ticket in tickets" :key="ticket.id">
+                <img :src="ticket.src" :alt="ticket.name">
+                <div>
+                    <button>+</button>
+                    <input type="number" name="0" id="tickNumber">
+                    <button>-</button>
+                </div>
+            </article>
+        </div>
+
+        <div class="tickMoney"> <!-- 可以改成資料庫名稱 -->
+            <p>票券金額</p>
+            <p>NT$ </p>
+            <h2>260</h2>
+        </div>
+
+        <div class="tickBtn">
+            <button>上一步</button>
+            <button class="tickLBtn">票種數量</button>
+        </div>
+
+    
+    </section>
+</template>
+
+<script>
+export default {
+    components:{
+        // RouterLink,
+
+    },
+    data() {
+        return {
+            isMobile: window.innerWidth <= 768,
+            tickets:[
+                {
+                    id: 1,
+                    name: '成人票',
+                    rule: '18~64 歲',
+                    price: 100,
+                    src: '/images/ticket/ticket1.svg'
+                },
+                {
+                    id: 2,
+                    name: '兒童票',
+                    rule: '4~11 歲',
+                    price: 80,
+                    src: '/images/ticket/ticket2.svg'
+                },
+                {
+                    id: 3,
+                    name: '學生票',
+                    rule: '12 歲以上(含)持學生證者',
+                    price: 40,
+                    src: '/images/ticket/ticket3.svg'
+                },
+                {
+                    id: 4,
+                    name: '愛心票',
+                    rule: '65 歲以上(含)',
+                    price: 4100,
+                    src: '/images/ticket/ticket4.svg'
+                },
+                {
+                    id: 5,
+                    name: '團體票',
+                    rule: '15 人以上適用',
+                    price: 60,
+                    src: '/images/ticket/ticket5.svg'
+                },
+            ]
+            // targetValue:0,
+        }
+    },
+    created(){
+        window.addEventListener('resize', this.windowSize);
+    },
+    methods:{
+        windowSize(){
+            this.isMobile = window.innerWidth <= 768;
+        },
+    }
+}
+
+</script>
+
+<style>
+
+
+
+</style>
--- src/views/TicketView.vue
+++ src/views/TicketView.vue
@@ -1,9 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This is an Ticket page</h1>
-  </div>
+  <section class="tick forheader">
+    <div class="tickStep">
+      <img src="/images/ticket/PC0.svg" alt="立即購票進度條">
+    </div>
+
+    <!-- <main>
+      <article v-if="isMobile">
+        <TickInfo/>
+      </article>
+      <div v-else class="tickJCSB">
+        <TickInfo/>
+        <TickCalendar/>
+      </div>
+    </main> -->
+
+    <main>
+        <TickNum/>
+    </main>
+
+    <!-- <div class="tickBtn">
+        <button>上一步</button>
+        <button class="tickLBtn">票種數量</button>
+    </div> -->
+    <!-- button 兩個都設定變數:
+        1. 隱藏這個componet
+        2. 渲染下一個componet-->
+    <!-- init PH的按鈕是選擇日期，PC的按鈕比他多一階段，是票種數量 -->
+    <!-- init PH的按鈕功能: 
+        1.隱藏購票須知
+        2.show進度條
+        3.渲染calender-->
+  </section>
 </template>
 
+<script>
+import TickInfo from '@/components/TickInfo.vue';
+import TickCalendar from '@/components/TickCalendar.vue';
+import TickNum from '@/components/TickNum.vue';
+
+export default {
+  components:{
+    // RouterLink,
+    TickInfo,
+    TickCalendar,
+    TickNum,
+  },
+  data() {
+    return {
+      isMobile: window.innerWidth <= 768,
+      // targetValue:0,
+    }
+    
+  },
+  created(){
+    window.addEventListener('resize', this.windowSize);
+  },
+  methods:{
+    windowSize(){
+      this.isMobile = window.innerWidth <= 768;
+    },
+  }
+}
+
+</script>
+
 <style>
+
+
 
 </style>
