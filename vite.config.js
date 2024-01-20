61ca521f5f5edcd001d769b0c4f878d449f82f35
yhkekeke <diandian2510@gmail.com>
2024-01-20 14:53:20 +0800

    部屬(add base&build in vite.config.js AND create .htaccess in public folder)

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+  ? '/chd104/g4/front/'
+  : '/',
+  build: {
+    outDir: 'front'
+  },
   plugins: [
     vue(),
   ],
