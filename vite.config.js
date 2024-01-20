14a9fb311d940927865f8cdc76c398a4bd128b54
SkerLeon <sker870812@gmail.com>
2024-01-20 15:30:09 +0800

    git設定front不會上傳 & 修改圖片路徑

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-  ? '/chd104/g4/front/'
+  ? '/chd104/ingrid/front/'
   : '/',
   build: {
     outDir: 'front'
