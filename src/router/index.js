53ce6b9ad8fba8358146f61e72d61cc012aad350
yhkekeke <diandian2510@gmail.com>
2024-01-20 14:07:51 +0800

    add pages' title in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,49 +8,80 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta:{
+        title:'PxZoO'
+      }
     },
     {
       path: '/ticket',
       name: 'ticket',
-      component: () => import('../views/TicketView.vue')
+      component: () => import('../views/TicketView.vue'),
+      meta:{
+        title:'立即購票 | PxZoO'
+      }
     },
     {
       path: '/member',
       name: 'member',
-      component: () => import('../views/MemberView.vue')
+      component: () => import('../views/MemberView.vue'),
+      meta:{
+        title:'會員中心 | PxZoO'
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue')
+      component: () => import('../views/LoginView.vue'),
+      meta:{
+        title:'登入註冊 | PxZoO'
+      }
     },
     {
       path: '/animal',
       name: 'animal',
-      component: () => import('../views/AnimalView.vue')
+      component: () => import('../views/AnimalView.vue'),
+      meta:{
+        title:'動物資訊 | PxZoO'
+      }
     },
     {
       path: '/park',
       name: 'park',
-      component: () => import('../views/ParkView.vue')
+      component: () => import('../views/ParkView.vue'),
+      meta:{
+        title:'園區介紹 | PxZoO'
+      }
     },
     {
       path: '/news',
       name: 'news',
-      component: () => import('../views/NewsView.vue')
+      component: () => import('../views/NewsView.vue'),
+      meta:{
+        title:'最新消息 | PxZoO'
+      }
     },
     {
       path: '/school',
       name: 'school',
-      component: () => import('../views/SchoolView.vue')
+      component: () => import('../views/SchoolView.vue'),
+      meta:{
+        title:'知識學堂 | PxZoO'
+      }
     },
     {
       path: '/vote',
       name: 'vote',
-      component: () => import('../views/VoteView.vue')
+      component: () => import('../views/VoteView.vue'),
+      meta:{
+        title:'人氣投票 | PxZoO'
+      }
     }
   ]
 })
-
+router.beforeEach(async (to, from) => {
+  if( to.meta && to.meta.title){
+      document.title = to.meta.title
+  }
+})
 export default router
