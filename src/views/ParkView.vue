9cbf3a0ab36708116d93664852737f609397c9e6
SkerLeon <sker870812@gmail.com>
2024-01-21 23:55:28 +0800

    門票資訊v-for建立

--- src/views/ParkView.vue
+++ src/views/ParkView.vue
@@ -19,7 +19,7 @@
       
       <div v-show="show_animals" class="pk_animal_icon">
         <div v-for="num in 38" :class="`pk_animal_default pk_animal_icon${num}`" :id="`location_id${num}`">
-          <img :src="getImageUrl(num)" alt="園區動物icon的圖片">
+          <img :src="getAnimalIconUrl(num)" alt="園區動物icon的圖片">
         </div>
       </div>
 
@@ -52,42 +52,19 @@
     <section class="pk_content_bg">
       <section class="pk_Tickets">
         <div class="pk_Tickets_items">
-          <div class="pk_Tickets_item">
-            <img class="pk_Tickets_item_top" src="@/assets/images/park/pk_Tickets_item_top1.png" alt="門票上半部">
-            <img class="pk_Tickets_item_mid" src="@/assets/images/park/pk_Tickets_item_mid1.png" alt="中間的擋板">
-            <img class="pk_Tickets_item_animal" src="@/assets/images/park/pk_Tickets_item_animal1.png" alt="中間的動物">
-            <img class="pk_Tickets_item_bottom" src="@/assets/images/park/pk_Tickets_item_bottom1.png" alt="門票下半部">
-          </div>
-
-          <div class="pk_Tickets_item">
-            <img class="pk_Tickets_item_top" src="@/assets/images/park/pk_Tickets_item_top2.png" alt="門票上半部">
-            <img class="pk_Tickets_item_mid" src="@/assets/images/park/pk_Tickets_item_mid2.png" alt="中間的擋板">
-            <img class="pk_Tickets_item_animal" src="@/assets/images/park/pk_Tickets_item_animal2.png" alt="中間的動物">
-            <img class="pk_Tickets_item_bottom" src="@/assets/images/park/pk_Tickets_item_bottom2.png" alt="門票下半部">
-          </div>
-
-          <div class="pk_Tickets_item">
-            <img class="pk_Tickets_item_top" src="@/assets/images/park/pk_Tickets_item_top3.png" alt="門票上半部">
-            <img class="pk_Tickets_item_mid" src="@/assets/images/park/pk_Tickets_item_mid3.png" alt="中間的擋板">
-            <img class="pk_Tickets_item_animal" src="@/assets/images/park/pk_Tickets_item_animal3.png" alt="中間的動物">
-            <img class="pk_Tickets_item_bottom" src="@/assets/images/park/pk_Tickets_item_bottom3.png" alt="門票下半部">
-          </div>
-
-          <div class="pk_Tickets_item">
-            <img class="pk_Tickets_item_top" src="@/assets/images/park/pk_Tickets_item_top4.png" alt="門票上半部">
-            <img class="pk_Tickets_item_mid" src="@/assets/images/park/pk_Tickets_item_mid4.png" alt="中間的擋板">
-            <img class="pk_Tickets_item_animal" src="@/assets/images/park/pk_Tickets_item_animal4.png" alt="中間的動物">
-            <img class="pk_Tickets_item_bottom" src="@/assets/images/park/pk_Tickets_item_bottom4.png" alt="門票下半部">
-          </div>
-
-          <div class="pk_Tickets_item">
-            <img class="pk_Tickets_item_top" src="@/assets/images/park/pk_Tickets_item_top5.png" alt="門票上半部">
-            <img class="pk_Tickets_item_mid" src="@/assets/images/park/pk_Tickets_item_mid5.png" alt="中間的擋板">
-            <img class="pk_Tickets_item_animal" src="@/assets/images/park/pk_Tickets_item_animal5.png" alt="中間的動物">
-            <img class="pk_Tickets_item_bottom" src="@/assets/images/park/pk_Tickets_item_bottom5.png" alt="門票下半部">
-          </div>
-
+          <RouterLink v-for="(ticket, index) in tickets" :key="index" to="/ticket" class="pk_Tickets_item">
+            <img class="pk_Tickets_item_top" :src="getItemTopUrl(index+1)" alt="門票上半部">
+            <img class="pk_Tickets_item_mid" :src="getItemMidUrl(index+1)" alt="中間的擋板">
+            <img class="pk_Tickets_item_animal" :src="getItemAnimalUrl(index+1)" alt="中間的動物">
+            <img class="pk_Tickets_item_bottom" :src="getItemBottomUrl(index+1)" alt="門票下半部">
+            <div class="pk_Tickets_item_text">
+              <h1 class="pcSmTitle">{{ticket.title}}</h1>
+              <p class="pcInnerText">{{ticket.content}}</p>
+              <p class="pcInnerText">{{ticket.content2}}</p>
+            </div>
+          </RouterLink>
         </div>
+        
       </section>
 
       <section class="pk_Origin">
@@ -106,6 +83,27 @@ export default {
       show_animals:true,
       show_facilities:true,
       hover : false,
+      tickets:[{
+        title:"成人票",
+        content:"18~64歲可使用",
+        content2:"NT$ 100",
+      },{
+        title:"兒童票",
+        content:"4~11歲可使用",
+        content2:"NT$ 40",
+      },{
+        title:"成人票",
+        content:"12歲以上持學生證",
+        content2:"NT$ 80",
+      },{
+        title:"愛心票",
+        content:"65歲以上可使用",
+        content2:"NT$ 40",
+      },{
+        title:"團體票",
+        content:"15人以上可使用",
+        content2:"NT$ 60",
+      }]
     };
   },
   created() {
@@ -115,8 +113,20 @@ export default {
     // 生命週期鉤子
   },
   methods: {
-    getImageUrl(paths){
+    getAnimalIconUrl(paths){
       return new URL(`../assets/images/park/pk_animal_icon${paths}.png`, import.meta.url).href
+    },
+    getItemTopUrl(paths){
+      return new URL(`../assets/images/park/pk_Tickets_item_top${paths}.png`, import.meta.url).href
+    },
+    getItemMidUrl(paths){
+      return new URL(`../assets/images/park/pk_Tickets_item_mid${paths}.png`, import.meta.url).href
+    },
+    getItemAnimalUrl(paths){
+      return new URL(`../assets/images/park/pk_Tickets_item_animal1${paths}.png`, import.meta.url).href
+    },
+    getItemBottomUrl(paths){
+      return new URL(`../assets/images/park/pk_Tickets_item_bottom${paths}.png`, import.meta.url).href
     },
     showAnimals(){
       this.show_animals = true
