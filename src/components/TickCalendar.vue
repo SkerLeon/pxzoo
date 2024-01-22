86cd43ea34a6b7f56f94bec6555d1a8a7f361a47
yhkekeke <diandian2510@gmail.com>
2024-01-22 21:00:28 +0800

    update ticket

--- src/components/TickCalendar.vue
+++ src/components/TickCalendar.vue
@@ -1,12 +1,31 @@
 <template>
+    <div>
+        <Calendar v-model="value" hide-type>
+            <template #month="{date, data}">
+                <div>
+                    <Badge status="warning" text="看苹果发布会" v-if="data.day === '2019-09-11'" />
+                    <Badge status="success" text="中秋节" v-if="data.day === '2019-09-13'" />
+                    <Badge status="error" text="国庆节" v-if="data.day === '2019-10-01'" />
+                </div>
+            </template>
+        </Calendar>
+    </div>
+
+
+
     <!-- part1 日曆 -->
-    <section class="tickCalendar">
+    <!-- <section class="tickCalendar">
 
         <article>
             <hgroup>
                 <h2>選擇日期</h2>
                 <img src="@/assets/images/ticket/refresh.svg">
             </hgroup>
+
+
+
+
+
             <article>
                 <img src="@/assets/images/ticket/PHcalender.svg" alt="" class="fakeCalendar">
             </article>
@@ -17,10 +36,14 @@
         </form>
 
     
-    </section>
+    </section> -->
 </template>
 
 <script>
+import { 
+    Calendar,Badge,
+} from 'view-ui-plus';
+
 export default {
     components:{
         // RouterLink,
