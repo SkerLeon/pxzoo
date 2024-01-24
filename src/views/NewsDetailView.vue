23739896a61349e359a798108fd9fe42d890d631
Ying-Jung189 <melody.c000.ron@gmail.com>
2024-01-24 08:54:33 +0800

    alter animal, news + connect animaldetail, newsdetail

--- src/views/NewsDetailView.vue
+++ src/views/NewsDetailView.vue
@@ -6,7 +6,29 @@
         </div>
 
         <main>
+            <div class="news_detail">
             
+                <div class="news_detail_info">
+                    <div class="news_detail_tag_time">
+                        <span class="time">2024.01.02</span>
+                        <span class="tag" >最新活動</span>
+                    </div>
+                    <div class="news_detail_title">
+                        <h4>動物明星新一輪投票即將啟動！</h4>
+                    </div>
+                </div>
+                <img src="../assets/images/news/decorate-line.png" alt="裝飾線" class="news_line upper">
+                <img src="../assets/images/news/new_activity_1.png" alt="news圖">
+                <div class="news_detail_text">
+                    <p>動物園即將迎來一場令人期待的「動物明星投票」活動！經過上一期12月的人氣投票，大象、長頸鹿和獅子成功脫穎而出，成為前三名的熱門動物明星。這次的活動將提供遊客們一個難得的機會，以票選心中最喜愛的動物，讓牠們成為動物園的代表性明星。</p>
+                    <p>
+                    參與投票的遊客不僅能夠支持自己心目中的動物候選人，還有機會贏得豐富的獎品。動物園希望透過這次活動，拉進與遊客之間的距離，同時讓大家更深入認識動物園的居民。</p>
+                    <p>
+                    這場「動物明星投票」活動將成為動物園最新、最受期待的亮點之一。歡迎大家踴躍參與，共同打造一個充滿歡笑和驚喜的動物園體驗！
+                    </p>
+                </div>
+
+            </div>
         </main>
     </section>
 </template>
@@ -16,39 +38,9 @@ export default {
     data() {
         return {
             
-            //select
-            newsCategory: [
-                {
-                    value: 'All',
-                    label: 'All'
-                },
-                {
-                    value: 'grassLand',
-                    label: '草原之聲'
-                },
-                {
-                    value: 'polar',
-                    label: '極地秘境'
-                },
-                {
-                    value: 'jungle',
-                    label: '叢林奇蹟'
-                },
-                {
-                    value: 'birds',
-                    label: '鳥園樂章'
-                },
-                {
-                    value: 'aqua',
-                    label: '海洋奇觀'
-                }
-            ],
         };
     },
     methods: {
-        getImageUrl(paths) {
-            return new URL(`../assets/images/animal/small_pic/small_pic_${paths}.png`, import.meta.url).href
-        },//改網址
     },
     created() {
     },
