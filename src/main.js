528564fa4ed50956ca8e12b762f5e2d4895c45dd
wenchieh0119 <jamie1063021@gmail.com>
2024-01-31 18:56:54 +0800

    update vote school add AOS

--- src/main.js
+++ src/main.js
@@ -10,7 +10,13 @@ import "./assets/scss/style.scss";
 
 import App from "./App.vue";
 import router from "./router";
+import AOS from 'aos';
+import 'aos/dist/aos.css';//導入AOS CSS 樣式
 // import VueNativeSock from 'vue-native-websocket';
+
+
+AOS.init();//初始化AOS
+
 
 // 創建一個vue應用程式(主要配置在app.vue裡)
 const vueApp = createApp(App);
