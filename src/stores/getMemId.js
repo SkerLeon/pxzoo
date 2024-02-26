5b74665b00fc9b12e4e97b958873299e4d77f6bf
yhkekeke <diandian2510@gmail.com>
2024-02-26 09:51:30 +0800

    update getMemId.js (if LocalStorage has empty userData)

--- src/stores/getMemId.js
+++ src/stores/getMemId.js
@@ -1,15 +1,17 @@
 export const getMemId = {
-    data() {
-        return {
-            mem_id: null,
+  data() {
+    return {
+      mem_id: null,
     };
-    },
-    created() {
-       // 從 local storage 取得 userData 字串
-        const userDataString = localStorage.getItem('userData');
-      // 將 userData 字串轉換為 JavaScript 物件
-        const userData = JSON.parse(userDataString);
-      // 從 JavaScript 物件中獲取 id 屬性
-        this.mem_id = userData ? userData.id : null;
-    },
+  },
+  created() {
+    // 從 local storage 取得 userData 字串
+    const userDataString = localStorage.getItem('userData');
+    // 將 userData 字串轉換為 JavaScript 物件
+    const userData = JSON.parse(userDataString);
+    // 判斷 userData 是有資料，還是空框架
+    let logInState = userData && typeof(userData.id)=='number';
+    // 從 JavaScript 物件中獲取 id 屬性
+    this.mem_id = logInState? userData.id : null;
+  },
 };
