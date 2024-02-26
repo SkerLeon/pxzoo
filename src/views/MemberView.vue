98ed1f8523dd000bf355a87d06eabbc99ebced8d
SkerLeon <sker870812@gmail.com>
2024-02-26 23:00:22 +0800

    修改switch顯示的圖片

--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -391,7 +391,7 @@ export default {
         case 1:
           imagePath = `${
             import.meta.env.VITE_IMAGES_BASE_URL
-          }/coupon/coupon_85.svg`;
+          }/coupon/coupon_95.svg`;
           break;
         case 2:
           imagePath = `${
@@ -401,7 +401,7 @@ export default {
         case 3:
           imagePath = `${
             import.meta.env.VITE_IMAGES_BASE_URL
-          }/coupon/coupon_95.svg`;
+          }/coupon/coupon_85.svg`;
           break;
       }
       return new URL(imagePath, import.meta.url).href;
